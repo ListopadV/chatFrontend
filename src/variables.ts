2bf1c6d2f7084a4070ce812465611341fad2e877
LVO <LVO@d.co>
2024-10-24 19:24:21 +0200

    changed backend url

--- src/variables.ts
+++ src/variables.ts
@@ -1,4 +1,4 @@
-export const url = 'https://chat-backend-taupe.vercel.app';
+export const url = 'https://chat-backend-vlo.vercel.app/';
 const github_client_id = process.env.REACT_APP_github_client_id;
 const redirectUri = process.env.REACT_APP_redirectUri
 export const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${github_client_id}&redirect_uri=${redirectUri}`
