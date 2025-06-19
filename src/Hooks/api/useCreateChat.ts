2c773ff526b17477935f42dc320cfadfbef26245
LVO <LVO@d.co>
2025-06-19 17:10:57 +0200

    fed

--- src/Hooks/api/useCreateChat.ts
+++ src/Hooks/api/useCreateChat.ts
@@ -2,6 +2,8 @@ import {createChat} from "../../services/chats";
 import {ChatDto as RequestChatDto} from "../../DTO's/requests/createChat";
 import useSWRMutation from "swr/mutation";
 import {CreatedChatEntity} from "../../Entities/ChatEntities";
+import {mutate} from "swr";
+import {idManager} from "../../services/auth/idManager";
 
 export const useCreateChat = () => {
     const { trigger, isMutating } = useSWRMutation<
@@ -11,7 +13,14 @@ export const useCreateChat = () => {
         RequestChatDto
     >(
         '/createChat',
-        async (_key, { arg }) => createChat(arg)
+        async (_key, { arg }) => {
+            const res = await createChat(arg);
+            if (res?.id){
+                 idManager.setChatId(res.id);
+                await mutate(['/currentChat', res.id]);
+            }
+            return res;
+        }
     )
 
     return { createChat: trigger, isCreating: isMutating }
